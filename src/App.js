2f827b79d9ccbafdfd8345d620af832bb1509177
acnologia <shrikanthbudya@gmail.com>
2018-06-13 14:56:12 +0530

    Cleaner code.

--- src/App.js
+++ src/App.js
@@ -2,36 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  state = {
-    list: [
-      "do something"
-    ],
-    value:''
-  }
-  render() {
-    console.log(this.state.list);
-    let list = [];
-    this.state.list.forEach((e, i) => {
-      list.push(<li className="todo" key={i} onClick={()=>alert(e)}>{e}</li>)
-    });
-    return (
-      <div className="App">
-        {list}
-        <input type="text" value={this.state.value} onChange={
-          ({ target: { value } }) => {
-            this.setState({ value: value })
-          }} />
-        <button onClick={
-          () => {
-            this.setState(prevState => ({
-              list: [...prevState.list, prevState.value],
-              value:''
-            }))
-          }
-        }> Add</button>
-      </div>
-    );
-  }
+	state = {
+		list: [
+			"do something"
+		],
+		value:''
+	}
+	addItem = () => {
+		this.setState(prevState => ({
+			list: [...prevState.list, prevState.value],
+			value: ''
+		}))
+	}
+	changeText = ({ target: { value } }) => {
+		this.setState({ value: value })
+	}
+	render() {
+		let list = this.state.list;
+		list = list.map((e, i) => {
+			return <li className="todo" key={i} onClick={()=>alert(e)}>{e}</li>
+		});
+		return (
+			<div className="App">
+				{list}
+				<input
+					type="text" value={this.state.value}
+					onChange={this.changeText} />
+				<button onClick={this.addItem}> Add</button>
+			</div>
+		);
+	}
 }
 
 export default App;
